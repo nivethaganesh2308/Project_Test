4bbfd5e83ad527167e974c82990e79eea1640ded
Nivetha <ganeshjeevan.n@husky.neu.edu>
2018-07-15 21:26:16 -0700

    Added a new python file to add numbers

---
+++ test_python.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+def add_two_numbers(a,b):
+"""
+:param: a : First Number
+:param: b : Second Number
+:return : Sum: Sum of the numbers
+"""
+c = a+b
+
+return c
+
+if __init__ == "__main__":
+    a = 30
+    b = 40
+    sum = add_two_numbers(a,b)
+    print("The sum is", sum)
